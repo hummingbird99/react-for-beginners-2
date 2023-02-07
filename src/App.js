674a2b9864092377696732b9a29a18be857421d4
hummingbird99 <sseoulite@gmail.com>
2023-02-08 02:10:30 +0900

    Feat: alt 속성 추가
    img 태그에 alt 속성 추가

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ function App() {
         <div>
           {movies.map((movie) => (
             <div key={movie.id}>
-              <img src={movie.medium_cover_image} />
+              <img src={movie.medium_cover_image} alt="movie poster" />
               <h2>{movie.title}</h2>
               <p>{movie.summary}</p>
               <ul>
